5c06d88aa9aa087f7e1d9175dba3e68a8576cd0a
filipef9 <filipef9@hotmail.com>
2022-03-23 14:09:29 +0000

    returns "Fizz" for multiples of 3

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 module.exports = num => {
     const numberIsMultipleOfThreeAndFive = num % 15 === 0;
     if (numberIsMultipleOfThreeAndFive) return 'FizzBuzz';
+
+    const numberIsMultipleOfThree = num % 3 === 0;
+    if (numberIsMultipleOfThree) return 'Fizz';
     
     return `${num}`;
 }
--- index.test.js
+++ index.test.js
@@ -10,4 +10,8 @@ describe('fizzBuzz()', () => {
         expect(fizzBuzz(1)).toBe('1');
         expect(fizzBuzz(22)).toBe('22');
     });
+
+    it('returns "Fizz" for multiples of 3', () => {
+        expect(fizzBuzz(27)).toBe('Fizz');
+    })
 });
